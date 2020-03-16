3cd28bc1c5e0150302cd0e676f1336e7f21b6542
Florian Langknecht <florian.langknecht@web.de>
2020-03-16 12:33:58 +0100

    Init, generic comment

---
+++ start-geode-he36.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+GFSH_DIR=/opt/apache-geode-1.11.0/bin
+$GFSH_DIR/gfsh run --file=start-geode-commands-he36.gfsh
---
+++ stop-geode-he36.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+GFSH_DIR=/opt/apache-geode-1.11.0/bin
+$GFSH_DIR/gfsh run --file=stop-geode-commands.gfsh
