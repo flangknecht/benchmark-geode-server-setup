ab103962d955e6866fd8e118033bb13990a563fc
Florian Langknecht <florian.langknecht@web.de>
2020-03-16 14:41:19 +0100

    Connection settings: update

--- start-geode-he37.sh
+++ start-geode-he37.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 GFSH_DIR=/opt/apache-geode-1.11.0/bin
-$GFSH_DIR/gfsh start server --name=server37-1 --server-bind-address=192.168.0.2 --server-port=40404 --locators=192.168.0.1[10334]
+$GFSH_DIR/gfsh start server --name=server37-1 --server-bind-address=192.168.0.2 --server-port=40404 --locators=192.168.0.1[10334] --max-connections=200
